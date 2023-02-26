9ef640708fa31f3c9ee2bb83f88f56abc7694ac2
glendonchin <glendonchin@gmail.com>
2023-02-26 15:48:13 -0500

    Redirect after registration

--- django_project/mysite/entries/views.py
+++ django_project/mysite/entries/views.py
@@ -3,6 +3,7 @@ from .models import Entry
 from .forms import EntryForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -27,6 +28,12 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            username=form.cleaned_data.get('username')
+            password =form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+
+            return redirect('home')
     else:
         form = UserCreationForm()
     context = {'form': form}
