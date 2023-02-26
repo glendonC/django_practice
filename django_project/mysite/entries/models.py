3cd8236f30db8525ed706e30a64c95fe3ddaac00
glendonchin <glendonchin@gmail.com>
2023-02-26 12:27:44 -0500

    Display entries from database

--- django_project/mysite/entries/models.py
+++ django_project/mysite/entries/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+# Has classes that represent tables in database
+
+class Entry(models.Model):
+    #attributes
+    text = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'Entry #{} '.format(self.id)
+
+    #Manages spelling
+    class Meta():
+        verbose_name_plural = 'entries'
+    
--- django_project/mysite/entries/views.py
+++ django_project/mysite/entries/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from .models import Entry
 
 # Create your views here.
 
 #pass name of the template
 def index(request):
-    return render(request, 'entries/index.html')
+    entries = Entry.objects.order_by('-date_posted')
+    context = {'entries': entries}
+    return render(request, 'entries/index.html', context)
 
 def add(request):
     return render(request, 'entries/add.html')
